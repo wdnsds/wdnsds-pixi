26a24e146e2425c91d9d00d0b36ec23c8e5b0881
wdnsds <rifqi.majid29@gmail.com>
2020-07-28 08:34:04 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+    <link rel="stylesheet" href="{{asset('css/landing_wbk.css')}}" type="text/css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pixi.js/4.7.1/pixi.min.js"></script>
+    <script src="{{asset('js/TweenMax.min.js')}}"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+<div id="pixiCanvas"></div>
+
+
+<h1 class="title">
+    <img src="{{asset('assets/black.png')}}" alt="">
+</h1>
+<h4 class="subtitle">WDNSDS</h4>
+
+
+
+
+<script>
+    $(document).ready(function(){
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+</script>
+</body>
+
+<script src="{{asset('js/js.js')}}"></script>
+</html>
